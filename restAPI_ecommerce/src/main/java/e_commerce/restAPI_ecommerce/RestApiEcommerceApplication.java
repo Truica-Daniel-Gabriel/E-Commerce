1d14bf116ce68b6058c3b28fe887e559a302de9a
Dani <truicadaniel@yahoo.com>
2023-11-14 14:29:46 +0200

    setup client server

---
+++ restAPI_ecommerce/src/main/java/e_commerce/restAPI_ecommerce/RestApiEcommerceApplication.java
@@ -0,0 +1,13 @@
+package e_commerce.restAPI_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiEcommerceApplication.class, args);
+	}
+
+}
