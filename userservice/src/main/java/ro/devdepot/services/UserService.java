9aa2bc5ec0374aaba25671d8033f0bbb19fce3ca
Dani <truicadaniel@yahoo.com>
2023-11-22 17:22:06 +0200

    #DD-18-Create-User-CRUD

--- userservice/src/main/java/ro/devdepot/services/UserService.java
+++ userservice/src/main/java/ro/devdepot/services/UserService.java
@@ -1,9 +1,20 @@
 package ro.devdepot.services;
 
 import ro.devdepot.model.User;
+import ro.devdepot.model.UserRole;
 import ro.devdepot.model.dto.LoginRequest;
 import ro.devdepot.model.dto.LoginResponse;
 
+import java.util.List;
+
 public interface UserService {
     public LoginResponse login(LoginRequest loginRequest);
+
+    User createUser(User user);
+    User updateUser(User user, Long id);
+    User updateUserRole(UserRole userRole, Long id);
+    User getUserById(Long id);
+    List<User> getAllUsers();
+    void deleteUserById(Long id);
+
 }
--- userservice/src/main/java/ro/devdepot/services/implementation/UserServiceImpl.java
+++ userservice/src/main/java/ro/devdepot/services/implementation/UserServiceImpl.java
@@ -1,10 +1,16 @@
 package ro.devdepot.services.implementation;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+import ro.devdepot.model.User;
+import ro.devdepot.model.UserRole;
 import ro.devdepot.model.dto.LoginRequest;
 import ro.devdepot.model.dto.LoginResponse;
 import ro.devdepot.repositories.UserRepository;
 import ro.devdepot.services.UserService;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -26,5 +32,43 @@ public class UserServiceImpl implements UserService {
         // if user.getRole.euals(UserRole.ADMIN)
 
         return null;
+    }
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(User user, Long id) {
+        User existingUser = userRepository.findById(id).orElse(null);
+
+        existingUser.setUsername(user.getUsername());
+
+        return userRepository.save(existingUser);
+    }
+
+    @Override
+    public User updateUserRole(UserRole newuserRole, Long id) {
+        User existingUser = userRepository.findById(id).orElse(null);
+
+        existingUser.setUserRole(newuserRole);
+
+        return userRepository.save(existingUser);
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public void deleteUserById(Long id) {
+        userRepository.deleteById(id);
     }
 }
